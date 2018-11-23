02de5270df4752cc0b54466a4ec188576a23facc
Nilad <abirer@chu-clermontferrand.fr>
2018-11-23 11:26:22 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+# setup.py file install libraries wanted for cnrphylotree
+# launch it with pip install -e .
+
+# Install setuptools if not already present.
+from setuptools import setup, find_packages
+import glob
+
+setup(
+    name='cnrphylotree',
+    version='1.0.1',
+    description='cnrphylotree: pipeline CNR Resistance for phylo tree',
+    packages=find_packages(),
+    author='Richard Bonnet',
+    author_email='rbonnet@chu-clermontferrand.fr',
+    url='https://github.com/CNRResistanceAntibiotic/CNRPhyloTree',
+    scripts=glob.glob('scripts/*'),
+    install_requires=[''],
+    license='GPLv3',
+    classifiers=[
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Programming Language :: Python :: 3 :: Only',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    ],
+
+)
