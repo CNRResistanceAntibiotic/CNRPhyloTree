c895a5dff1e12db4e6da0784364fc14d470cec6e
Nilad <abirer@chu-clermontferrand.fr>
2018-11-23 10:49:43 +0100

    First commit

---
+++ cnr_phylo_tree.py
@@ -0,0 +1,393 @@
+#!/usr/bin/python3
+
+import argparse
+import multiprocessing
+import os
+import subprocess
+import sys
+from csv import DictReader
+
+from skbio import DistanceMatrix
+from skbio.tree import nj
+
+from cnr_phylo_tree_src import filter_SNP_density, vcf2dist, mtx2mst
+
+
+def load_config(config_file):
+    config_list = []
+
+    with open(config_file, "r") as conf:
+        reader = DictReader(conf, delimiter=",")
+        config_list = list(reader)
+
+    return config_list
+
+
+def run_snippy(snippy_exe, threads, out_dir, ref_genome, r1_seq_file, r2_seq_file):
+
+    cmd = "{0} --cpus {1} --outdir {2} --reference {3} --R1 {4} --R2 {5} ".format(snippy_exe, threads, out_dir, ref_genome,
+                                                                              r1_seq_file, r2_seq_file)
+    log_message = " ".join(cmd)
+    print(cmd)
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    log_message = log_message + '\n' + out.decode("utf-8") + '\n' + err.decode("utf-8")
+
+    print(log_message)
+
+    return log_message
+
+
+def run_snippy_core_custom(snippy_exe, ref_genome, prefix, snippy_folder):
+    cmd = '{0} --ref {1} --prefix {2} {3} '.format(snippy_exe, ref_genome, prefix, snippy_folder)
+    print(cmd)
+    os.system(cmd)
+
+
+def manage_snippy(read_dir, geno_ref_dir, result_dir, config_list):
+    print("\n")
+
+    snippy_exe = "/usr/local/snippy/bin/snippy"
+    threads = 8
+
+    strain_list = os.listdir(read_dir)
+
+    snippy_dir_dict = {}
+    jobs = []
+
+    for row in config_list:
+        genome_name = row["genomes"].split(".")[0]
+        out_dir = os.path.join(result_dir, genome_name, row['strains'])
+
+        r1_seq_file = ""
+        r2_seq_file = ""
+
+        ref_genome = os.path.join(geno_ref_dir, row["genomes"])
+        if not os.path.exists(ref_genome):
+            print("your reference genome file {0} don't exists or we haven't the permission\n".format(ref_genome))
+            print(usage)
+            sys.exit()
+
+        for strain in strain_list:
+            if row['strains']+"_" in strain:
+                if "R1" in strain:
+                    r1_seq_file = os.path.join(read_dir, strain)
+
+                    if not os.path.exists(r1_seq_file):
+                        print("the fastq file {0} don't exists or we haven't the permission\n".format(
+                            r1_seq_file))
+                        print(usage)
+                        sys.exit()
+
+                if "R2" in strain:
+                    r2_seq_file = os.path.join(read_dir, strain)
+
+                    if not os.path.exists(r2_seq_file):
+                        print("the fastq file {0} don't exists or we haven't the permission\n".format(
+                            r2_seq_file))
+                        print(usage)
+                        sys.exit()
+
+        if not r1_seq_file:
+            print("The sequence file {0} is not found".format(r1_seq_file))
+            print("The program continue but without the strain  {0}".format(row['strains']))
+            continue
+
+        if not r2_seq_file:
+            print("The sequence file {0} is not found".format(r1_seq_file))
+            print("The program continue but without the strain  {0}".format(row['strains']))
+            continue
+
+        if ref_genome not in snippy_dir_dict:
+            snippy_dir_dict[ref_genome] = [out_dir]
+        else:
+            out_dir_list = snippy_dir_dict[ref_genome]
+
+            out_dir_list.append(out_dir)
+
+            # update dict
+            snippy_dir_dict[ref_genome] = out_dir_list
+
+        if not os.path.exists(out_dir):
+
+            p = multiprocessing.Process(
+                target=run_snippy,
+                args=(snippy_exe, threads, out_dir, ref_genome, r1_seq_file, r2_seq_file),
+                name='snippy {0}'.format(row['strains'])
+            )
+            jobs.append(p)
+
+        else:
+            print("The snippy folder {0} already exist !".format(out_dir))
+
+    job_list = []
+    threshold = 3
+    count = 1
+
+    # launch and wait multiple jobs
+    for job in jobs:
+        job_list.append(job)
+        if count == threshold:
+            # launch jobs
+            for job_ready in job_list:
+                # remove element
+                jobs.remove(job_ready)
+                # start
+                job_ready.start()
+            # wait jobs
+            for job_ready in job_list:
+                job_ready.join()
+                job_list = []
+                count = 0
+        count += 1
+
+    # launch rest of jobs
+    for job in jobs:
+        job.start()
+
+    # wait rest of jobs
+    for job in jobs:
+        job.join()
+
+    return snippy_dir_dict
+
+
+def manage_snippy_core(snippy_dir_dict):
+    snippy_exe = "/usr/local/snippy/bin/snippy-core"
+    name_dir = "core_genome"
+    vcf_list = []
+    print("\n")
+
+    for genome_ref, snippy_dir_list in snippy_dir_dict.items():
+        snippy_dirs = " ".join(snippy_dir_list)
+
+        # case with only one sample that is not respectable !
+        if len(snippy_dir_list) ==1:
+            continue
+
+        core_genome_path = os.path.join(os.path.dirname(snippy_dir_list[0]), name_dir)
+
+        if not os.path.exists(core_genome_path):
+            os.mkdir(core_genome_path)
+            prefix_snippy_core = os.path.join(core_genome_path, name_dir)
+            run_snippy_core_custom(snippy_exe, genome_ref, prefix_snippy_core, snippy_dirs)
+        else:
+            print("The core genome folder produces by snippy-core already exist : {0}".format(core_genome_path))
+
+    for file in os.listdir(core_genome_path):
+        if "{0}.vcf".format(name_dir) in file:
+            vcf_list.append(os.path.join(core_genome_path, file))
+
+    return vcf_list
+
+
+def manage_filter_snp(vcf_list):
+    min_dist = 20
+    out_prefix = "filter"
+    filter_keep_vcf_list = []
+
+    for vcf_core_file in vcf_list:
+
+        if not os.path.exists(os.path.join(os.path.dirname(vcf_core_file),
+                                           out_prefix + "_" +
+                                           os.path.basename(vcf_core_file).split(".vcf")[0] +
+                                           "_density_filtered_keep.vcf")):
+
+            filter_SNP_density.main(min_dist, vcf_core_file, out_prefix)
+
+        else:
+            print("the filtration is already done for {0}".format(vcf_core_file))
+
+        for file in os.listdir(os.path.dirname(vcf_core_file)):
+
+            if "_filtered_keep.vcf" in file:
+                filter_keep_vcf_list.append(os.path.join(os.path.dirname(vcf_core_file), file))
+
+    return filter_keep_vcf_list
+
+
+def manage_r_matrix(filter_keep_vcf_list):
+    r_matrix_list = []
+
+    for filter_keep_vcf in filter_keep_vcf_list:
+        vcf2dist.main(filter_keep_vcf)
+
+        for file in os.listdir(os.path.dirname(filter_keep_vcf)):
+
+            if "_filtered_keep_SNP_dist.tsv" in file:
+                r_matrix_list.append(os.path.join(os.path.dirname(filter_keep_vcf), file))
+
+    return r_matrix_list
+
+
+def manage_make_tree(filter_keep_vcf_list):
+
+
+    for filter_keep_vcf in filter_keep_vcf_list:
+
+        with open(filter_keep_vcf, "r") as conf:
+            reader = DictReader(conf, delimiter="\t")
+            headers = reader.fieldnames
+
+            # remove empty col
+            headers=list(filter(None, headers))
+
+            matrix = []
+            for row in reader:
+                line = []
+                for col in headers:
+
+                    value = row[col]
+                    line.append(value)
+                matrix.append(line)
+
+            dm = DistanceMatrix(matrix, headers)
+
+            print(dm)
+
+            tree = nj(dm)
+
+            print(tree.ascii_art())
+
+            newick_str = nj(dm, result_constructor=str)
+            print(newick_str)
+
+            print(newick_str[:55], "...")
+
+
+def manage_snp_network(r_matrix_list):
+    for mtx_file in r_matrix_list:
+        graph_name = os.path.join(os.path.dirname(mtx_file), "SNP_network.html")
+        st_file = ""
+
+        mtx2mst.main(mtx_file, graph_name, st_file)
+
+
+def pre_main(args):
+    read_dir = ""
+    geno_ref_dir = ""
+    result_dir = ""
+    config_file = ""
+
+    if args.repRead:
+        read_dir = os.path.abspath(args.repRead)
+
+    if args.genomeRef:
+        geno_ref_dir = os.path.abspath(args.genomeRef)
+
+    if args.repResult:
+        result_dir = os.path.abspath(args.repResult)
+
+    if args.config:
+        config_file = os.path.abspath(args.config)
+
+    if not os.path.exists(read_dir):  # if the folder of reads exist, continue
+        print("your path of directory reads don't exists or we haven't the permission\n")
+        print(usage)
+        sys.exit()
+
+    if not os.path.exists(geno_ref_dir):  # if the folder of genome exist, continue
+        print("your reference genome directory don't exists or we haven't the permission\n")
+        print(usage)
+        sys.exit()
+
+    elif not os.path.isdir(geno_ref_dir):
+        print("""\nYou have enter a file and not the folder which contain genome(s),
+                with the option configfile it isn't right\n""")
+        sys.exit()
+
+    if not os.path.exists(config_file):  # if the folder of genome exist, continue
+        print("your config file don't exists or we haven't the permission\n")
+        print(usage)
+        sys.exit()
+
+    if not os.path.exists(result_dir):
+        print("Result directory was created at '{0}'".format(result_dir))
+        os.makedirs(result_dir)
+
+    main(read_dir, geno_ref_dir, result_dir, config_file)
+
+
+def main(read_dir, geno_ref_dir, result_dir, config_file):
+    """
+    This make a configuration file with argument for launch of snakemake
+    :param read_dir:
+    :param geno_ref_dir:
+    :param result_dir:
+    :param config_file:
+    """
+
+    config_list = load_config(config_file)
+
+    # snippy
+    print("\nStart Snippy")
+    snippy_dir_dict = manage_snippy(read_dir, geno_ref_dir, result_dir, config_list)
+    print("End Snippy")
+    print("*********************************************")
+
+    # snippy_core
+    print("\nStart Snippy-core")
+    vcf_list = manage_snippy_core(snippy_dir_dict)
+    print("End Snippy-core")
+    print("*********************************************")
+
+    # filter SNP
+    print("\nStart filtering SNP")
+    filter_keep_vcf_list = manage_filter_snp(vcf_list)
+    print("End filtering SNP")
+    print("*********************************************")
+
+    # Rmatrix
+    print("\nStart distance matrix")
+    r_matrix_list = manage_r_matrix(filter_keep_vcf_list)
+    print("End distance matrix")
+    print("*********************************************")
+
+
+    # Make tree
+    print("\nStart make tree")
+    manage_make_tree(r_matrix_list)
+    print("End make tree")
+    print("*********************************************")
+
+    # networkX
+    print("\nStart networkX")
+    manage_snp_network(r_matrix_list)
+    print("End networkX")
+
+
+def run():
+    """
+    Take the argument of the command and give a variable for this
+    :return: args
+    """
+
+    global usage
+
+    usage = "CNR Phylo tree is a snakemake based programm"
+
+    parser = argparse.ArgumentParser(
+        description="This program make a phylogenetic tree with NGS sequence reads and one or more reference genome")
+    parser.add_argument('-i', '--repRead', dest="repRead", default='',
+                        help='Enter the path of the directory which contain your fasta/fastq/fas/fa(.gz) reads')
+    parser.add_argument('-o', '--repResult', dest="repResult", default='',
+                        help="""Enter the path of your directory where you want your result (without other result or
+                        risk of nothing that will be done)""")
+    parser.add_argument('-g', '--repGenomeRef', dest="genomeRef", default='',
+                        help="""Enter the path of directory with reference genome
+                        (if no configfile enter the path to your reference genome)""")
+    parser.add_argument('-c', '--config', dest="config", default='', help="Enter the path of your csv file which "
+                                                                          "config the association of genome with "
+                                                                          "strain (help of csv2json.py for see how "
+                                                                          "make this csv file)")
+
+    args = parser.parse_args()
+    pre_main(args)
+
+
+def version():
+    return "1.0"
+
+
+if __name__ == '__main__':
+    run()
---
+++ cnr_phylo_tree_src/vcf2dist.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python3
+import os
+import argparse
+from collections import OrderedDict
+import vcf
+from itertools import combinations
+import pandas as pd
+
+
+def read_vcf(vcf_file):
+    print("\nRead vcf file {}".format(vcf_file))
+    vcf_dic = OrderedDict()
+    vcf_reader = vcf.Reader(open(vcf_file, 'r'))
+    sample_names = vcf_reader.samples
+    for record in vcf_reader:
+        if record.CHROM not in vcf_dic.keys():
+            vcf_dic[record.CHROM] = [record]
+        else:
+            vcf_dic[record.CHROM].append(record)
+    print("Number of loci   : {}".format(len(vcf_dic)))
+    print("Number of samples: {}".format(len(sample_names)))
+    return vcf_dic, sample_names
+
+
+def vcf2dist(vcf_dic, sample_names):
+    print("\nConvert VCF data in SNP and MLST distances")
+    contig_dist_dict = OrderedDict()
+    snp_dist_dict = OrderedDict()
+    for sample1 in sample_names:
+        if sample1 not in contig_dist_dict.keys():
+            contig_dist_dict[sample1] = OrderedDict()
+            snp_dist_dict[sample1] = OrderedDict()
+        for sample2 in sample_names:
+            contig_dist_dict[sample1][sample2] = 0
+            snp_dist_dict[sample1][sample2] = 0
+
+    for chrom in vcf_dic.keys():
+        records = vcf_dic[chrom]
+        mlst = OrderedDict()
+        for sample1, sample2 in combinations(sample_names, 2):
+            if sample1 not in mlst.keys():
+                mlst[sample1] = OrderedDict({sample2: 0})
+            else:
+                mlst[sample1][sample2] = 0
+
+        # snp distance
+        for record in records:
+            data = {}
+            for sample1, sample2 in combinations(sample_names, 2):
+                # print sample1, record.genotype(sample1)['GT'], sample2, record.genotype(sample2)['GT']
+                if record.genotype(sample1)['GT'] != record.genotype(sample2)['GT']:
+                    # print 'sample1 diff sample2'
+                    s1_genotype = str(record.ALT[int(record.genotype(sample1)['GT'].split('/')[0]) - 1])
+                    s2_genotype = str(record.ALT[int(record.genotype(sample2)['GT'].split('/')[0]) - 1])
+                    # print s1_genotype, s2_genotype
+                    if s1_genotype != 'N' or s2_genotype != 'N':
+                        # print 'diff of N'
+                        snp_dist_dict[sample1][sample2] = snp_dist_dict[sample1][sample2] + 1
+                        snp_dist_dict[sample2][sample1] = snp_dist_dict[sample1][sample2]
+                        mlst[sample1][sample2] = mlst[sample1][sample2] + 1
+                # else:
+                #	print "%s %s %s %i: N genotype excluded in SNP count" % (sample1, sample2, chrom, record.POS)
+
+        # contig distance (unscoop to snp distance)
+        for sample1, sample2 in combinations(sample_names, 2):
+            if mlst[sample1][sample2] > 0:
+                contig_dist_dict[sample1][sample2] = contig_dist_dict[sample1][sample2] + 1
+                contig_dist_dict[sample2][sample1] = contig_dist_dict[sample1][sample2]
+
+    return snp_dist_dict, contig_dist_dict
+
+
+def write_dist(dist_dic, dist_type, out_prefix):
+    df = pd.DataFrame.from_records(dist_dic)
+    outfile = out_prefix + '_%s_dist.tsv' % dist_type
+    df.to_csv(outfile, sep='\t')
+    print("{0} distance matrix written in {1}".format(dist_type, outfile))
+
+
+def pre_main(args):
+    vcf_file = os.path.abspath(args.vcf)
+    main(vcf_file)
+
+
+def main(vcf_file):
+    out_prefix = os.path.splitext(vcf_file)[0]
+
+    vcf_dic, sample_names = read_vcf(vcf_file)
+    snp_dist_dict, contig_dist_dict = vcf2dist(vcf_dic, sample_names)
+
+    write_dist(snp_dist_dict, 'SNP', out_prefix)
+    write_dist(contig_dist_dict, 'Contig', out_prefix)
+    print('\nvcf to matrix distances done!\n')
+
+
+def version():
+    return '1.0'
+
+
+def run():
+    parser = argparse.ArgumentParser(description='vcf2dist - Version ' + version())
+    parser.add_argument('-v', '--vcf', dest="vcf", default='output_filtered.vcf', help='vcf file [output_filtered.vcf]')
+    parser.add_argument('-V', '--version', action='version', version='vcf2dist-' + version(),
+                        help="Prints version number")
+    args = parser.parse_args()
+    pre_main(args)
+
+
+if __name__ == '__main__':
+    run()
